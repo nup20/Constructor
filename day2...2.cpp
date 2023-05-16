b9cfff250bf02de5f8f5fa01212de0c3a39402e1
nup20 <130826435+nup20@users.noreply.github.com>
2023-05-16 18:41:03 +0530

    Add files via upload

---
+++ day2...2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Student
+{
+public:
+    string name,ss;
+
+
+    Student()
+    {
+        name="unknown";
+        cout<<" Name :"<< name<<endl;
+
+    }
+
+    Student(string a)
+    {
+         ss=a;
+         cout<<"Enter Name :"<<ss<<endl;
+    }
+
+
+};
+int main()
+{
+string a;
+
+     Student S,s1("sam");
+
+     return 0;
+}
